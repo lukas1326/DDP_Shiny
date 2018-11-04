8e15ddef2bde94c4179e523f61e7118f96de8e9a
lukas <aprilium@live.com>
2018-11-04 21:07:44 +0200

    add summary and instruction

--- Volcano/server.R
+++ Volcano/server.R
@@ -30,7 +30,7 @@ server <- function(input, output, session) {
                
                 leaflet() %>%
                         
-                        addProviderTiles(providers$Stamen.TonerLite,
+                        addProviderTiles(providers$Esri.NatGeoWorldMap,
                                          options = providerTileOptions(noWrap = TRUE)
                         ) %>%
                         addCircleMarkers(lng=data$Longitude,
--- Volcano/ui.R
+++ Volcano/ui.R
@@ -14,7 +14,22 @@ vol <- read.csv("data/volcano.csv")
 vol <- vol %>% filter(!vol$V_Name=="Unnamed")
 
 ui <- fluidPage(
-        
+        titlePanel("Global Volcano Model"),
+        HTML("A comprehensive set of information on global volcanic hazard, 
+historical events, population exposure, 
+vulnerability, and impact has been provided to GAR15 by Global Volcano Model (GVM)
+and The International Association of Volcanology and 
+Chemistry of the Earth’s Interior (IAVCEI). 
+This work is the first of its kind in global coverage and level of contribution 
+from a wide network of experts and institutions around the world.
+<br>Source:https:\\data.humdata.org/dataset/volcano-population-exposure-index-gvm
+
+             "),
+        HTML("<br><br> Instruction: <br> 
+                 1. Choose in selection field the country <br> 
+                 2. Explore the map zoomed by volcano place <br>
+                 3. The purple or red dots are volcano's place. The red one is for highest risk <br>
+                 4. The table below shows the subset from the original data by country <br>"),
         selectInput("country",
                     "Countries:",
                 choices = unique(sort(vol$Country))),
